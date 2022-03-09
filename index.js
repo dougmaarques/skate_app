06fcf5c48ae846bdffe7cf1babf1ed89909d421d
Douglas Marques <douglas_marques_araujo@hotmail.com>
2022-03-09 15:18:31 -0300

    Adicionando novas páginas e arrumando funcionalidades

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const botaoMenu = document.querySelector('.cabecalho__menu')
 const menu = document.querySelector('.menu-lateral')
+const botaoUser = document.querySelector('.cabecalho__perfil')
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 
+})
+
+botaoUser.addEventListener('click',() => {
+    menu.classList.toggle('cabecalho__perfil_menu-ativo')
 })
